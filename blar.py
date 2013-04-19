acdf174bde44d4043fe99c8bf51519e6ddc2c31e
Joseph Godlewski <joe3701@gmail.com>
2013-04-18 23:34:05 -0700

    added cool function

--- blar.py
+++ blar.py
@@ -3,3 +3,12 @@ def f1(n):
         return n * 3
     else:
         return 0
+
+def coolFn(x = 3) :
+    q = 0
+    for i in range(x):
+        q = q + i
+    return q
+
+if __name__ == "__main__":
+    print coolFn(f1(3**3))
